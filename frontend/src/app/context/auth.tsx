9204f22fd806e6581a4f93fab6c7f3704a1bb99a
AnastasiiaMuzh <anastasiia.muzhzhavlev@outlook.com>
2025-10-16 12:32:38 -0600

    Mergi

---
+++ frontend/src/app/context/auth.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { User } from "../../types/user";
+import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
+
+
+type AuthContextType = {
+  user: User | null;
+  setUser: (user: User) => void;
+  isLoggedIn: boolean;
+  logout: () => void;
+  loginWithToken: (token: string, user?: User) => void;
+  token: string | null;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
+    if (storedUser) setUser(JSON.parse(storedUser));
+    if (storedToken) setToken(storedToken);
+  }, []);
+
+  //save user
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  //save token
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
+  const loginWithToken = (token: string, user?: User) => {
+    setToken(token);
+    if (user) {
+      setUser(user);
+    }
+    // If the backend doesn't return a user, you can decode the token and extract user information.
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, isLoggedIn: !!user, logout, loginWithToken, token }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+
+export function useAuth() {
+  const ctx = useContext(AuthContext);
+  if (!ctx) throw new Error("useAuth must be used within AuthProvider");
+  return ctx;
+}
