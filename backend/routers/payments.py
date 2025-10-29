0a3500ed53dd34e3d8fecaaf31afcdc632e57dab
Andrew <andrew.lizon12@gmail.com>
2025-10-29 11:42:50 -0700

    Refactored webhook, create_payments, added StripeEvents model

--- backend/routers/payments.py
+++ backend/routers/payments.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, status, HTTPException, Request
+from fastapi import APIRouter, status, HTTPException, Depends, Request
+from typing import Annotated
+from .auth.login import get_current_user
+from models.user_models import User
 from models.interaction_models import DonationCreate
 from db.prisma_client import db
 import stripe
@@ -11,6 +14,7 @@ STRIPE_WEBHOOK_SECRET = os.getenv("STRIPE_WEBHOOK_SECRET")
 @router.post("", status_code=status.HTTP_202_ACCEPTED)
 async def create_payment(
     donation_data: DonationCreate,
+    current_user: Annotated[User, Depends(get_current_user)],
 ):
     """
         Create Donation
@@ -19,11 +23,17 @@ async def create_payment(
 
     """
 
+    # Custom data to add to Stripe Event
+    metadata = {}
+    if current_user:
+        metadata["userId"] = str(current_user.id)
+        metadata["donationType"] = donation_data.donationType
+
     try:
         session = stripe.checkout.Session.create(
             mode="payment",
             invoice_creation={"enabled": True},
-             line_items=[
+            line_items=[
                 {
                     "price_data": {
                         "currency": "usd",
@@ -37,6 +47,7 @@ async def create_payment(
             ],
             ui_mode="embedded",
             return_url="https://whproject.org",
+            metadata=metadata
         )
         return {
             "client-secret": session.client_secret,
@@ -57,16 +68,39 @@ async def stripe_webhook(request: Request):
     except:
         raise HTTPException(status_code=400, detail="Invalid signature")
     
+    # Check for existing event -> idempotent
+    existing_event = await db.stripeevents.find_unique(
+        where={"eventId": event["id"]}
+    )
+
+    if existing_event:
+        return {"status": "duplicate_ignored"}
+
+    # Create event
+    await db.stripeevents.create(data={"eventId": event["id"]})
+
     if event["type"] == "checkout.session.completed":
         session = event["data"]["object"]
-    
+        
+        # Check for existing donation -> idempotent       
+        existing_donation = await db.donations.find_unique(
+            where={"sessionId": session["id"]}
+        )
+
+        user_id = session["metadata"].get("userId")
+
+        # Create donation
+        if existing_donation:
+            return {"status": "duplicate_ignored"}
+        
         donation_data = {
-            "amount": session["amount_total"]/100,
-            "currency": session["currency"] ,
+            "donationType": session["metadata"]["donationType"],
+            "amount": int(session["amount_total"] / 100),
             "email": session.get("customer_email"),
-            "session_id": session["id"]
-        }
+            "sessionId": session["id"],
+            "user": {"connect": {"id": user_id}} if user_id else None,
+            }
 
         await db.donations.create(data=donation_data)
 
-        return {"status": "success"}
+    return {"status": "success"}
