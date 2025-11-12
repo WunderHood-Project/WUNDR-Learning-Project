a620e96c3958e4f5db4bfee720678c669d3318b2
Andrew <andrew.lizon12@gmail.com>
2025-11-11 20:16:49 -0800

    Connected support us page to tax-return

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -4,13 +4,13 @@
 export default function Footer() {
   return (
     <footer className="w-full bg-wondergreen text-white border-t py-10 mt-10">
-        <div className="flex flex-wrap justify-center text-lg gap-6 mb-4">
-            <a href="/support" className="hover:underline">Support Us</a>
-            <a href="/volunteer" className="hover:underline">Volunteer</a>
-            <a href="/partners" className="hover:underline">Partner With Us</a>
-            <a href="/contact" className="hover:underline">Contact Us</a>
-            <div className="flex justify-center gap-4 mb-2">
-            {/* <a href="https://linkedin.com/-link" target="_blank" rel="noopener noreferrer">
+      <div className="flex flex-wrap justify-center text-lg gap-6 mb-4">
+        <a href="/tax-return" className="hover:underline">Support Us</a>
+        <a href="/volunteer" className="hover:underline">Volunteer</a>
+        <a href="/partners" className="hover:underline">Partner With Us</a>
+        <a href="/contact" className="hover:underline">Contact Us</a>
+        <div className="flex justify-center gap-4 mb-2">
+          {/* <a href="https://linkedin.com/-link" target="_blank" rel="noopener noreferrer">
                 <FaLinkedin  className="w-6 h-6 hover:opacity-80" />
             </a>
             <a href="https://instagram.com/-link" target="_blank" rel="noopener noreferrer">
@@ -21,15 +21,15 @@ export default function Footer() {
             </a> */}
         </div>
 
-        </div>
+      </div>
 
 
       <div className="border-t border-gray-200 pt-4 max-w-5xl mx-auto text-center text-white text-md">
-      <p className="mb-3">WonderHood is a registered 501(c)(3) non-profit organization. All donations are tax-deductible to the fullest extent allowed by law.</p>
+        <p className="mb-3">WonderHood is a registered 501(c)(3) non-profit organization. All donations are tax-deductible to the fullest extent allowed by law.</p>
         <div className="flex flex-col md:flex-row items-center justify-center gap-2 md:gap-6 text-md md:text-md">
-            <span>&copy; 2025 WonderHood. All rights reserved.</span>
-            <a href="/privacy-policy" className="hover:underline">Privacy Policy</a>
-            <a href="/terms" className="hover:underline">Terms of Service</a>
+          <span>&copy; 2025 WonderHood. All rights reserved.</span>
+          <a href="/privacy-policy" className="hover:underline">Privacy Policy</a>
+          <a href="/terms" className="hover:underline">Terms of Service</a>
         </div>
       </div>
     </footer>
